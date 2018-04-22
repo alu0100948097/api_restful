ca8bd274897de1e74a930ea80ffc2bb5a9d1d5ec
alu0100948097 <alu0100948097@ull.edu.es>
2018-04-22 12:41:40 +0100

    Implementación de la api

--- metodos.js
+++ metodos.js
@@ -4,23 +4,60 @@ var fs = require('fs');
 var server = restify.createServer();
 var Texto = "";
 
-server.get('api/productos/all', (req,res,next) =>
+server.get('/asignaturas/all', (req,res,next) =>
 {
-	var file = fs.readFileSync('./data.txt');
-	res.send(200,'Hola mundo');
+    res.setHeader('Content-Type', 'application/json');
+    var file = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    res.json(file);
 	next();
 });
 
-server.get('/api/productos/add/:valor', (req,res,next) =>
+
+server.get('/asignaturas/add/:valor', (req,res,next) =>
 {
-	res.send("El valor " +req.params,valor+ "ha sido añadido!");
+    var value = req.params.valor.toString();
+    var file = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    for (var i = 0; i < file.length; i++)
+           if (i==file.length-1)
+                var n_nid = file[i].nid+1
+    var tmp = { nid: n_nid, valor: value }
+    file.push(tmp);
+    fs.writeFile('data.json', JSON.stringify(file));
+    res.send("El valor " +value+ " ha sido añadido!");
 	next();
 });
 
-server.get('/api/productos/delete/:nid', (req,res,next) =>
+server.get('/asignaturas/update/:nid/:valor', (req,res,next) =>
 {
-	var id = req.params.nid;
-	next();
+    var id = req.params.nid;
+    var value = req.params.valor.toString();
+    var file = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    for (var i = 0; i < file.length; i++)
+           if(file[i].nid==parseInt(id))
+                file[i].valor=value
+    fs.writeFile('data.json', JSON.stringify(file));
+    res.send("El id " +id+ " ha sido modificado por el valor " +value+ "!");
+    next();
+});
+
+server.get('/asignaturas/delete/:nid', (req,res,next) =>
+{
+    var id = req.params.nid;
+    var file = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    var n_file = [];
+    var j = 0;
+    for (var i = 0; i < file.length; i++)
+    {
+           if(file[i].nid!=parseInt(id))
+           {
+                n_file.push(file[i]);
+                n_file[j].nid=j+1;
+                j+=1;
+           }
+    }
+    fs.writeFile('data.json', JSON.stringify(n_file));
+    res.send("El id " +id+ " ha sido eliminado!");
+    next();
 });
 
 server.listen(8888, function()
@@ -28,7 +65,7 @@ server.listen(8888, function()
 	console.log('%s escuchando en el puerto %s', server.name, server.url);
 });
 
-server.on('uncaughtException', (req,res,route,err) => 
+server.on('uncaughtException', (req,res,route,err) =>
 {
 	err.body = 'He pillado una excepcion :-(';
 	res.send(500,err);
